ae8660de18fe1ed3b6b1138458b7b52ec7249194
Mauro Zenoni <zenonim@student.ethz.ch>
2021-01-18 14:19:01 +0100

    Improved readibility of main function

--- src/Main.ts
+++ src/Main.ts
@@ -6,18 +6,16 @@ export async function main(): Promise<void> {
     const office = new Building("53.339428", "-6.257664");
     const radius = 100e3; // 100 km
 
-    const selectedCustomers: Customer[] = [];
+    const matchingCustomers: Customer[] = [];
 
-    return customers
-        .forEach(customer => {
-            if (office.distance(customer) <= radius) selectedCustomers.push(customer)
-        }).then(() =>
-            writeToFile(
-                "output.txt",
-                selectedCustomers
-                    .sort((customer1, customer2) => customer1.user_id - customer2.user_id)
-                    .map(customer => `${customer.name}: ${customer.user_id}`)
-                    .join("\n")
-            )
-        );
+    await customers.forEach(customer => {
+        if (office.distance(customer) <= radius) matchingCustomers.push(customer);
+    });
+
+    const output: string = matchingCustomers
+        .sort((customer1, customer2) => customer1.user_id - customer2.user_id)
+        .map(customer => `${customer.name}: ${customer.user_id}`)
+        .join("\n");
+
+    return writeToFile("output.txt", output);
 }
